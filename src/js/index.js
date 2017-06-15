f24f912471e0b5603767074d2775af09a83f4288
nerdmanship <johan@nerdmanship.com>
2017-06-15 19:26:06 +0800

    Add mobile intro

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,22 @@
 // @codekit-prepend "space.js";
 // @codekit-prepend "stats.js";
 
+var space = document.querySelector("[data-anim=space]");
 var planet = document.querySelector("[data-anim=planet]");
 
-TweenMax.set(planet, { autoAlpha: 1, y: 2100, x: 50, scale: 4, rotation: 2, transformOrigin: "center" });
-TweenMax.from(planet, 10, {y: 4200, x: -1000, scale: 7, rotation: -50, ease: Back.easeOut.config(0.5), onComplete: idle});
+// check screenwidth
+var vw = window.innerWidth;
 
-function idle() {
-  //TweenMax.to(planet, 5, { y: 2120, repeat: -1, yoyo: true, ease: Power1.easeInOut });
-  console.log("idle");
+// Start settings
+TweenMax.set(planet, { autoAlpha: 1, rotation: 2, transformOrigin: "center" });
+
+TweenMax.to(space, 1, {autoAlpha: 1});
+
+if (vw<500) {
+  // Mobile intro
+  TweenMax.set(planet, { y: 800, x: 50, scale: 1.4 });
+} else {
+  // Desktop intro
+  TweenMax.set(planet, { y: 2100, x: 50, scale: 4 });
+  TweenMax.from(planet, 10, {y: 4200, x: -1000, scale: 7, rotation: -20, ease: Back.easeOut.config(0.5)});
 }
