73d37ebef10f4f013147483d1cc85332793f5760
nerdmanship <johan@nerdmanship.com>
2017-06-30 23:14:43 +0200

    add dynamic artwork

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 // @codekit-prepend "../assets/nerdmanship/js/utility.js";
-// @codekit-prepend "space.js";
 // @codekit-prepend "stats.js";
 // @codekit-prepend "planetBryan.js";
 
-initPlanetBryan();
+initPlanetBryan("scene");
--- src/js/planetBryan.js
+++ src/js/planetBryan.js
@@ -1,55 +1,69 @@
 // @codekit-prepend "Flame.js";
 // @codekit-prepend "flicker.js";
+// @codekit-prepend "svg.js";
 
-var initPlanetBryan = function() {
-  // Create scene
-    // Create space
-    // Inject SVG
-
-  // Cache DOM
-  var scene = document.querySelector("[data-anim=scene]");
-  var planet = document.querySelector("[data-anim=planet]");
-  var fireplace = document.querySelector("[data-anim=fireplace]");
-
-  // Set start values
-  var vw = window.innerWidth;
-  TweenMax.set(planet, { autoAlpha: 1, transformOrigin: "center" });
-
-  // Reveal scene
-  TweenMax.to(scene, 1, {autoAlpha: 1});
-
-  // Start animation
-  startFire(fireplace);
-
-  if (vw<500) {
-    // Mobile experience
-    TweenMax.set(planet, { y: 1000, x: 50, scale: 1.8 });
-  } else {
-    // Desktop experience
-    flicker();
-    particlesJS.load('space', 'js/config.json');
-    TweenMax.set(planet, { y: 2100, x: 50, scale: 4 });
-    TweenMax.from(planet, 20, {y: 4200, x: -1000, scale: 7, rotation: -30, ease: Back.easeOut.config(0.5)});
-  }
+var initPlanetBryan = function(id) {
+  buildSceneIn(id);
+  animateScene();
 };
 
+function buildSceneIn(id) {
+  var container = document.querySelector("#" + id);
 
-function startFire(parent) {
-  // create
-  var fire = document.createElementNS("http://www.w3.org/2000/svg", "g");
+  // Create and insert #space
+  var spaceWrapper = document.createElement("div");
+  spaceWrapper.setAttribute("id", "space");
+  container.appendChild(spaceWrapper);
+
+  // Create and insert #planet-wrapper
+  var planetWrapper = document.createElement("div");
+  planetWrapper.setAttribute("id", "planet-wrapper");
+  container.appendChild(planetWrapper);
+
+  // Append SVG in #planet-wrapper
+  planetWrapper.insertAdjacentHTML("beforeend", svg);
+}
+
+function animateScene() {
   
-  // populate
+  var planetBody = document.querySelector("[data-anim=planetBody]");
+
+  if (window.innerWidth < 321) {
+    TweenMax.set(planetBody, { y: 1000, x: 50, scale: 1.8, transformOrigin: "center" });
+    startFire();
+    createSpace();
+  } else {
+    TweenMax.set(planetBody, { y: 2100, x: 50, scale: 4, transformOrigin: "center" });
+    startFire();
+    flicker();
+    createSpace();
+    TweenMax.from(planetBody, 20, {y: 4200, x: -1000, scale: 7, rotation: -30, ease: Back.easeOut.config(0.5)});
+  }
+
+}
+
+
+function startFire() {
+  var fireplace = document.querySelector("[data-anim=fireplace]");
+  var fireGroup = document.createElementNS("http://www.w3.org/2000/svg", "g");
   var count = 60;
 
   for(var i = 0; i < count; i++) {
+    // Create new flame
     var flame = new Flame();
-    flame.appendTo(fire);
+    // Add flame to group
+    flame.appendTo(fireGroup);
+    // Make every 10th flame into sparkle
     if (i%10 === 0) flame.isSparkle();
+    // Animate flame
     flame.animate();
+    // Set unique starting poing of flame
     flame.tl.progress(1/count*i).play().timeScale(0.75);
   }
 
+  fireplace.appendChild(fireGroup);
+}
 
-  // append
-  parent.appendChild(fire);
+function createSpace() {
+  particlesJS.load('space', 'js/config.json');
 }
